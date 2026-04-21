c574937dfdc31f41f2f7349b9910e048d32e5d41
Maxime06000 <maxime.lescouzeres@gmail.com>
2026-04-21 11:52:05 +0200

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['pdfjs-dist'],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'pdfjs-dist': 'pdfjs-dist/legacy/build/pdf.mjs',
-      }
-    }
-    return config
-  },
+  serverExternalPackages: ['pdfjs-dist'],
 };
 
 export default nextConfig;
